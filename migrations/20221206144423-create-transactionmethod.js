11f86bb85240cc9a0bd858c7bddd7f00aa1fb218
riganrahmadin <riganrahmadin8@gmail.com>
2022-12-07 00:22:21 +0700

    ganti

--- migrations/20221206144423-create-transactionmethod.js
+++ migrations/20221206144423-create-transactionmethod.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('transactionmethods', {
+    await queryInterface.createTable('Transactionmethods', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('transactionmethods');
+    await queryInterface.dropTable('Transactionmethods');
   }
 };
--- models/transactionmethod.js
+++ models/transactionmethod.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class transactionmethod extends Model {
+  class Transactionmethod extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  transactionmethod.init({
+  Transactionmethod.init({
     name: DataTypes.STRING,
     accountNumber: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'transactionmethod',
+    modelName: 'Transactionmethod',
   });
-  return transactionmethod;
+  return Transactionmethod;
 };
